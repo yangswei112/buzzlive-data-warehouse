604d7e56afd12fbc62ba2a4dc6684a78292a2ea0
Yayang Nurul Aulia Azizah <111848032+yangswei112@users.noreply.github.com>
2025-12-25 11:33:10 +0700

    Update loaddatabronze.sql

--- scripts/loaddatabronze.sql
+++ scripts/loaddatabronze.sql
@@ -1,43 +1,56 @@
 -- Insert data into bronze schema tables
+USE BuzzliveWarehouse;
+GO
+CREATE PROCEDURE bronze.load_info AS
+BEGIN
+	-- Insert data into brand_info table
+	TRUNCATE TABLE bronze.brand_info;
+	BULK INSERT bronze.brand_info
+	FROM 'C:\Users\ASUS\Downloads\reporting\brand data\brand_table.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
--- Insert data into brand_info table
-BULK INSERT bronze.brand_info
-FROM 'C:\Users\ASUS\Downloads\reporting\brand data\brand_table.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	-- Insert data into host_info table
+	TRUNCATE TABLE bronze.host_info;
+	BULK INSERT bronze.host_info
+	FROM 'C:\Users\ASUS\Downloads\reporting\host data\host_table.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+END;
 
--- Insert data into host_info table
-BULK INSERT bronze.host_info
-FROM 'C:\Users\ASUS\Downloads\reporting\host data\host_table.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+GO
 
--- Insert data into shopee_livestreaming table
-BULK INSERT bronze.shopee_livestreaming
-FROM 'C:\Users\ASUS\Downloads\reporting\result\shopee_ready_to_db.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+CREATE PROCEDURE bronze.load_shopee AS
+BEGIN
+	-- Insert data into shopee_livestreaming table
+	--TRUNCATE TABLE bronze.shopee_livestreaming;
+	BULK INSERT bronze.shopee_livestreaming
+	FROM 'C:\Users\ASUS\Downloads\reporting\result\ready_to_db_shopee.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK,
+		CODEPAGE = 'ACP'
+	);
+END;
 
--- Insert data into tiktok_livestreaming table
-BULK INSERT bronze.tiktok_livestreaming
-FROM 'C:\Users\ASUS\Downloads\reporting\result\tiktok_ready_to_db.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
--- CHECK
-SELECT * FROM bronze.brand_info
-SELECT * FROM bronze.host_info
-SELECT * FROM bronze.shopee_livestreaming
-SELECT * FROM bronze.tiktok_livestreaming
+GO
+ 
+CREATE PROCEDURE bronze.load_tiktok AS
+BEGIN
+	-- Insert data into tiktok_livestreaming table
+	--TRUNCATE TABLE bronze.tiktok_livestreaming;
+	BULK INSERT bronze.tiktok_livestreaming
+	FROM 'C:\Users\ASUS\Downloads\reporting\result\ready_to_db_tiktok.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+END;
