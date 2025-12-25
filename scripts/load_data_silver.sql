01851e78238bc894012f32b80223917801b2c081
Yayang Nurul Aulia Azizah <111848032+yangswei112@users.noreply.github.com>
2025-12-25 11:34:05 +0700

    Update load_data_silver.sql

--- scripts/load_data_silver.sql
+++ scripts/load_data_silver.sql
@@ -1,126 +1,136 @@
 -- Insert data into silver table
+USE BuzzliveWarehouse;
+GO
+CREATE PROCEDURE silver.load_info AS
+BEGIN
+	-- Insert data into brand_table
+	TRUNCATE TABLE silver.brand_info;
+	INSERT INTO silver.brand_info (
+		brand_id,
+		brand_name,
+		platform)
 
--- Insert data into brand_table
-
-INSERT INTO silver.brand_info (
+	SELECT
 	brand_id,
 	brand_name,
-	platform)
+	platform
+	FROM bronze.brand_info;
 
-SELECT
-brand_id,
-brand_name,
-platform
-FROM bronze.brand_info;
+	-- Insert data into host_table
+	TRUNCATE TABLE silver.host_info;
+	INSERT INTO silver.host_info (
+		host_id,
+		host_name,
+		host_birthdate)
 
--- Insert data into host_table
+	SELECT
+		host_id,
+		host_name,
+		host_birthdate
+	FROM bronze.host_info;
+END;
 
-INSERT INTO silver.host_info (
-	host_id,
-	host_name,
-	host_birthdate)
+GO
 
-SELECT
-	host_id,
-	host_name,
-	host_birthdate
-FROM bronze.host_info;
+CREATE PROCEDURE silver.load_shopee AS
+BEGIN
+	-- INSERT INTO SHOPEE_LIVESTREAMING
+	TRUNCATE TABLE silver.shopee_livestreaming;
+	INSERT INTO silver.shopee_livestreaming (
+		UserId,
+		LivestreamName,
+		live_host_id, 
+		live_start, 
+		live_start_date, 
+		live_start_time,
+		live_duration, 
+		live_engaged_viewers, 
+		live_comments, 
+		live_atc,
+		live_viewers, 
+		live_placed_orders, 
+		live_confirmed_orders,
+		live_placed_items_sold, 
+		live_confirmed_items_sold,
+		live_avg_viewing_duration,
+		live_placed_sales,
+		live_confirmed_sales
+	)
+	SELECT
+		UserId,
+		LivestreamName,
+		live_host_id, 
+		live_start, 
+		live_start_date, 
+		live_start_time,
+		live_duration, 
+		live_engaged_viewers, 
+		live_comments, 
+		live_atc,
+		live_viewers, 
+		live_placed_orders, 
+		live_confirmed_orders,
+		live_placed_items_sold, 
+		live_confirmed_items_sold,
+		live_avg_viewing_duration,
+		live_placed_sales,
+		live_confirmed_sales
 
--- INSERT INTO SHOPEE_LIVESTREAMING
-INSERT INTO silver.shopee_livestreaming (
-	UserId,
-	LivestreamName,
-	live_host_id, 
-	live_start, 
-	live_start_date, 
-	live_start_time,
-	live_duration, 
-	live_engaged_viewers, 
-	live_comments, 
-	live_atc,
-	live_viewers, 
-	live_placed_orders, 
-	live_confirmed_orders,
-	live_placed_items_sold, 
-	live_confirmed_items_sold,
-	live_avg_viewing_duration,
-	live_placed_sales,
-	live_confirmed_sales
-)
-SELECT
-	UserId,
-	LivestreamName,
-	live_host_id, 
-	live_start, 
-	live_start_date, 
-	live_start_time,
-	live_duration, 
-	live_engaged_viewers, 
-	live_comments, 
-	live_atc,
-	live_viewers, 
-	live_placed_orders, 
-	live_confirmed_orders,
-	live_placed_items_sold, 
-	live_confirmed_items_sold,
-	live_avg_viewing_duration,
-	live_placed_sales,
-	live_confirmed_sales
+	FROM bronze.shopee_livestreaming;
+END;
 
-FROM bronze.shopee_livestreaming;
+GO
 
--- INSERT INTO TIKTOK_LIVESTREAMING
-INSERT INTO silver.tiktok_livestreaming (
+CREATE PROCEDURE silver.load_tiktok AS
+BEGIN
+	-- INSERT INTO TIKTOK_LIVESTREAMING
+	TRUNCATE TABLE silver.tiktok_livestreaming;
+	INSERT INTO silver.tiktok_livestreaming (
+		CreatorId, 
+		LivestreamCreator, 
+		live_direct_gmv,
+		OrdersPaidFor, 
+		ItemsSold, 
+		Customers, 
+		live_avg_price, 
+		live_gross_revenue, 
+		Viewers, 
+		Views, 
+		AvgViewDuration, 
+		Comments,
+		Shares, 
+		Likes, 
+		NewFollowers, 
+		ProductImpressions,
+		ProductClicks,  
+		live_start_date, 
+		live_start_time,
+		live_duration, 
+		live_ctor, 
+		live_ctr
+	)
+	SELECT
 	CreatorId, 
-	LivestreamCreator, 
-	live_direct_gmv,
-	OrdersPaidFor, 
-	ItemsSold, 
-	Customers, 
-	live_avg_price, 
-	live_gross_revenue, 
-	Viewers, 
-	Views, 
-	AvgViewDuration, 
-	Comments,
-	Shares, 
-	Likes, 
-	NewFollowers, 
-	ProductImpressions,
-	ProductClicks,  
-	live_start_date, 
-	live_start_time,
-	live_duration, 
-	live_ctor, 
-	live_ctr
-)
-SELECT
-CreatorId, 
-	LivestreamCreator, 
-	live_direct_gmv,
-	OrdersPaidFor, 
-	ItemsSold, 
-	Customers, 
-	live_avg_price, 
-	live_gross_revenue, 
-	Viewers, 
-	Views, 
-	AvgViewDuration, 
-	Comments,
-	Shares, 
-	Likes, 
-	NewFollowers, 
-	ProductImpressions,
-	ProductClicks,  
-	live_start_date, 
-	live_start_time,
-	live_duration, 
-	live_ctor, 
-	live_ctr
-FROM bronze.tiktok_livestreaming
-
--- CHECK
-SELECT * FROM silver.brand_info
-SELECT * FROM silver.host_info
-SELECT * FROM silver.shopee_livestreaming
-SELECT * FROM silver.tiktok_livestreaming
+		LivestreamCreator, 
+		live_direct_gmv,
+		OrdersPaidFor, 
+		ItemsSold, 
+		Customers, 
+		live_avg_price, 
+		live_gross_revenue, 
+		Viewers, 
+		Views, 
+		AvgViewDuration, 
+		Comments,
+		Shares, 
+		Likes, 
+		NewFollowers, 
+		ProductImpressions,
+		ProductClicks,  
+		live_start_date, 
+		live_start_time,
+		live_duration, 
+		live_ctor, 
+		live_ctr
+	FROM bronze.tiktok_livestreaming;
+END;
