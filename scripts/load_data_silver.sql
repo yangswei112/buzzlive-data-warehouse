5f2cb17b7ac150216d2047b53db43054213a320f
Yayang Nurul Aulia Azizah <111848032+yangswei112@users.noreply.github.com>
2026-01-12 17:00:30 +0700

    Update load_data_silver.sql

--- scripts/load_data_silver.sql
+++ scripts/load_data_silver.sql
@@ -8,12 +8,14 @@ BEGIN
 	INSERT INTO silver.brand_info (
 		brand_id,
 		brand_name,
-		platform)
+		platform,
+		category)
 
 	SELECT
 	brand_id,
 	brand_name,
-	platform
+	platform,
+	category
 	FROM bronze.brand_info;
 
 	-- Insert data into host_table
@@ -34,103 +36,29 @@ GO
 
 CREATE PROCEDURE silver.load_shopee AS
 BEGIN
-	-- INSERT INTO SHOPEE_LIVESTREAMING
-	TRUNCATE TABLE silver.shopee_livestreaming;
-	INSERT INTO silver.shopee_livestreaming (
-		UserId,
-		LivestreamName,
-		live_host_id, 
-		live_start, 
-		live_start_date, 
-		live_start_time,
-		live_duration, 
-		live_engaged_viewers, 
-		live_comments, 
-		live_atc,
-		live_viewers, 
-		live_placed_orders, 
-		live_confirmed_orders,
-		live_placed_items_sold, 
-		live_confirmed_items_sold,
-		live_avg_viewing_duration,
-		live_placed_sales,
-		live_confirmed_sales
-	)
-	SELECT
-		UserId,
-		LivestreamName,
-		live_host_id, 
-		live_start, 
-		live_start_date, 
-		live_start_time,
-		live_duration, 
-		live_engaged_viewers, 
-		live_comments, 
-		live_atc,
-		live_viewers, 
-		live_placed_orders, 
-		live_confirmed_orders,
-		live_placed_items_sold, 
-		live_confirmed_items_sold,
-		live_avg_viewing_duration,
-		live_placed_sales,
-		live_confirmed_sales
-
-	FROM bronze.shopee_livestreaming;
+	-- Insert data into shopee_livestreaming table
+	--TRUNCATE TABLE bronze.shopee_livestreaming;
+	BULK INSERT silver.shopee_livestreaming
+	FROM 'C:\Users\ASUS\Downloads\reporting\result\ready_to_db_shopee_silver.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK,
+		CODEPAGE = 'ACP'
+	);
 END;
 
 GO
-
+ 
 CREATE PROCEDURE silver.load_tiktok AS
 BEGIN
-	-- INSERT INTO TIKTOK_LIVESTREAMING
-	TRUNCATE TABLE silver.tiktok_livestreaming;
-	INSERT INTO silver.tiktok_livestreaming (
-		CreatorId, 
-		LivestreamCreator, 
-		live_direct_gmv,
-		OrdersPaidFor, 
-		ItemsSold, 
-		Customers, 
-		live_avg_price, 
-		live_gross_revenue, 
-		Viewers, 
-		Views, 
-		AvgViewDuration, 
-		Comments,
-		Shares, 
-		Likes, 
-		NewFollowers, 
-		ProductImpressions,
-		ProductClicks,  
-		live_start_date, 
-		live_start_time,
-		live_duration, 
-		live_ctor, 
-		live_ctr
-	)
-	SELECT
-	CreatorId, 
-		LivestreamCreator, 
-		live_direct_gmv,
-		OrdersPaidFor, 
-		ItemsSold, 
-		Customers, 
-		live_avg_price, 
-		live_gross_revenue, 
-		Viewers, 
-		Views, 
-		AvgViewDuration, 
-		Comments,
-		Shares, 
-		Likes, 
-		NewFollowers, 
-		ProductImpressions,
-		ProductClicks,  
-		live_start_date, 
-		live_start_time,
-		live_duration, 
-		live_ctor, 
-		live_ctr
-	FROM bronze.tiktok_livestreaming;
+	-- Insert data into tiktok_livestreaming table
+	--TRUNCATE TABLE bronze.tiktok_livestreaming;
+	BULK INSERT silver.tiktok_livestreaming
+	FROM 'C:\Users\ASUS\Downloads\reporting\result\ready_to_db_tiktok.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 END;
