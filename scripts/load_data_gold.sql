3020504fbb87fcb9c64bb5f29c53b10071c41b31
Yayang NAAW <yayang1123@gmail.com>
2026-01-29 15:03:17 +0700

    edit

--- scripts/load_data_gold.sql
+++ scripts/load_data_gold.sql
@@ -1,5 +1,6 @@
 USE BuzzliveWarehouse;
 
+GO
 -- CREATE VIEWS FOR WEEKLY REPORTING
 CREATE VIEW gold.WeeklyShopeeLive AS
 SELECT
@@ -23,6 +24,8 @@ ON sl.UserId = bi.brand_id
 LEFT JOIN silver.host_info hi
 ON sl.live_host_id = hi.host_id;
 
+GO
+
 CREATE VIEW gold.WeeklyTiktokLive AS
 SELECT
 tl.Studio AS Studio,
@@ -42,12 +45,16 @@ FROM silver.tiktok_livestreaming tl
 LEFT JOIN silver.brand_info bi
 ON tl.CreatorId = bi.brand_id;
 
+GO
+
 -- FOR HR
 CREATE VIEW gold.ShopeeRawDataForHR AS
 SELECT sl.LivestreamName, sl.StartTime, sl.Duration, sl.live_start_date, bi.brand_name, bi.platform 
 FROM bronze.shopee_livestreaming sl
 LEFT JOIN bronze.brand_info bi
 ON sl.UserId = bi.brand_id;
+
+GO
 
 CREATE VIEW gold.TiktokRawDataForHR AS
 SELECT tl.LivestreamCreator, tl.StartTime, tl.Duration, tl.live_start_date, bi.brand_name, bi.platform 
