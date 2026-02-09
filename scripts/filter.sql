66e18136abe53823f6c0efa2dc451671dac41f8a
Yayang NAAW <yayang1123@gmail.com>
2026-02-09 14:58:40 +0700

    edit tokobig & abadi logam

--- scripts/filter.sql
+++ scripts/filter.sql
@@ -261,7 +261,7 @@ BEGIN
     -- TOKOBIG
     UPDATE silver.shopee_livestreaming
     SET Studio = CASE 
-        WHEN live_start_time > '08:00:00' THEN 'Klaten'
+        WHEN live_start_time BETWEEN '16:00:00' AND '20:59:00' THEN 'Klaten'
         ELSE 'Client' 
     END
     WHERE UserId = (SELECT brand_id FROM silver.brand_info WHERE brand_name='TOKOBIG')
@@ -287,7 +287,10 @@ BEGIN
 
     -- Abadi Logam
     UPDATE silver.shopee_livestreaming
-    SET Studio = 'Klaten'
+    SET Studio = CASE 
+        WHEN live_start_time BETWEEN '20:00:00' AND '23:59:00' THEN 'Client'
+        ELSE 'Klaten' 
+    END
     WHERE UserId = (SELECT brand_id FROM silver.brand_info WHERE brand_name='Abadi Logam' AND platform='Shopee')
     AND live_start_date BETWEEN @start_date AND @end_date
 
