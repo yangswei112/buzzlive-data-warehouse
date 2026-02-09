02b21ab9a839d64a0331978ddcbefc8517a16ac8
Yayang NAAW <yayang1123@gmail.com>
2026-02-09 15:26:23 +0700

    edit adebah

--- scripts/filter.sql
+++ scripts/filter.sql
@@ -168,7 +168,7 @@ BEGIN
     UPDATE silver.shopee_livestreaming
     SET Studio = CASE 
         --WHEN live_start_time < '16:00:00' THEN 'Klaten'
-        WHEN live_start_time BETWEEN '06:00:00' AND '09:59:00' THEN 'Klaten'
+        WHEN live_start_time BETWEEN '07:00:00' AND '13:59:00' THEN 'Klaten'
         --WHEN live_start_time BETWEEN '14:00:00' AND '15:59:00' THEN 'Klaten'
         ELSE 'Client' 
     END
