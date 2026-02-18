809e79381291004ec2d3e75966822adf41102751
Yayang NAAW <yayang1123@gmail.com>
2026-02-18 10:09:14 +0700

    modify main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,16 +4,16 @@ import os
 
 # SET UP THE FILE PATH
 
-shopee_report_path = 'C:/Users/ASUS/Downloads/reporting/shopee seller center/adds/'
-tiktok_report_path = 'C:/Users/ASUS/Downloads/reporting/tiktok seller center/jan-week3/'
-tiktok_report_path_2 = 'C:/Users/ASUS/Downloads/reporting/tiktok seller center 2/jan-week3/'
-result_path = 'C:/Users/ASUS/Downloads/reporting/result/'
+shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/feb-week2/'
+tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/tiktok seller center/feb-week2/'
+# tiktok_report_path_2 = 'C:/Users/ASUS/Downloads/reporting/tiktok seller center 2/jan-week3/'
+result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/result/'
 
 # TRANSFORM SHOPEE LIVESTREAMING
-shopee_transform_action(shopee_report_path)
+shopee_transform_action(shopee_report_path, result_path)
 
 # TRANSFORM TIKTOK LIVESTREAMING
-tiktok_transform_action(tiktok_report_path)
+tiktok_transform_action(tiktok_report_path, result_path)
 
 
 
