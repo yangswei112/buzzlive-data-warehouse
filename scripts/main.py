6d610d191e4a625617ecff9c675667ea94b237a7
Yayang NAAW <yayang1123@gmail.com>
2026-02-18 10:18:42 +0700

    modify main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ import os
 
 # SET UP THE FILE PATH
 
-shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/feb-week2/'
+shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/feb-week2-2/'
 tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/tiktok seller center/feb-week2/'
 # tiktok_report_path_2 = 'C:/Users/ASUS/Downloads/reporting/tiktok seller center 2/jan-week3/'
 result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/result/'
@@ -13,7 +13,7 @@ result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datase
 shopee_transform_action(shopee_report_path, result_path)
 
 # TRANSFORM TIKTOK LIVESTREAMING
-tiktok_transform_action(tiktok_report_path, result_path)
+# tiktok_transform_action(tiktok_report_path, result_path)
 
 
 
