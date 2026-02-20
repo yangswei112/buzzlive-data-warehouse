5cce5078b1f2d81b3114d735323136468240528d
Yayang NAAW <yayang1123@gmail.com>
2026-02-20 09:54:05 +0700

    add new function

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,25 @@
 from buzzwh_transform import *
 import pandas as pd
 import os
+import pyodbc
+
+# SET UP THE DATABASE CONNECTION
+connection_string = ("DRIVER={SQL Server};PORT=1433;SERVER=LAPTOP-Q4096V85\SQLEXPRESS;DATABASE=BuzzliveWarehouse;Trusted_Connection=yes;")
 
 # SET UP THE FILE PATH
-
-# shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/feb-week2-2/'
+shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/feb-week2-2/'
 tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/tiktok seller center/adds/'
-# tiktok_report_path_2 = 'C:/Users/ASUS/Downloads/reporting/tiktok seller center 2/jan-week3/'
 result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/result/'
 
 # TRANSFORM SHOPEE LIVESTREAMING
-# shopee_transform_action(shopee_report_path, result_path)
+shopee_transform_action(shopee_report_path, result_path)
 
 # TRANSFORM TIKTOK LIVESTREAMING
 tiktok_transform_action(tiktok_report_path, result_path)
 
+# UPDATE SHOPEE MONTHLY SALES
+update_shopee_sales(shopee_report_path, connection_string)
 
+# UPDATE TIKTOK MONTHLY SALES
+update_tiktok_sales(tiktok_report_path, connection_string)
 
