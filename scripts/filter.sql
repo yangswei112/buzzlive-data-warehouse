2e735154c3fa5e15965e085ab71404e7ad9995a2
Yayang NAAW <yayang1123@gmail.com>
2026-03-16 14:02:57 +0700

    edit filter

--- scripts/filter.sql
+++ scripts/filter.sql
@@ -432,7 +432,7 @@ BEGIN
     -- VIDYA OUTLET
     UPDATE silver.shopee_livestreaming
     SET Studio = CASE 
-        WHEN live_start_time BETWEEN '08:00:00' AND '09:59:00' THEN 'Klaten'
+        WHEN live_start_time BETWEEN '06:00:00' AND '09:59:00' THEN 'Klaten'
         WHEN live_start_time BETWEEN '18:00:00' AND '21:59:00' THEN 'Klaten'
         ELSE 'Client' 
     END
