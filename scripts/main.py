547a9fb575ea84e4fccd7d7121f3aed68447cd80
Yayang NAAW <yayang1123@gmail.com>
2026-04-09 10:19:35 +0700

    modify

--- scripts/main.py
+++ scripts/main.py
@@ -7,8 +7,8 @@ import pyodbc
 connection_string = ("DRIVER={SQL Server};PORT=1433;SERVER=LAPTOP-Q4096V85\SQLEXPRESS;DATABASE=BuzzliveWarehouse;Trusted_Connection=yes;")
 
 # SET UP THE FILE PATH
-shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/adds/'
-# tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/tiktok seller center/apr-week1/'
+# shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/shopee seller center/adds/'
+tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/tiktok seller center/adds/'
 result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/datasets/result/'
 
 # SET UP DATE RANGE
@@ -16,8 +16,8 @@ start_date = '2024-03-01'
 end_date = '2024-03-31'
 
 # # TRANSFORM DATA
-shopee_transform_action(shopee_report_path, result_path)
-# tiktok_transform_action(tiktok_report_path, result_path)
+# shopee_transform_action(shopee_report_path, result_path)
+tiktok_transform_action(tiktok_report_path, result_path)
 
 # # LOAD DATA TO DATABASE
 # load_to_db(connection_string, start_date, end_date)
