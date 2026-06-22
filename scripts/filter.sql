8502abf1df88a285b11463f6011fa3edbca994e4
Yayang NAAW <yayang1123@gmail.com>
2026-06-22 13:08:09 +0700

    update func

--- scripts/filter.sql
+++ scripts/filter.sql
@@ -92,9 +92,9 @@ BEGIN
      -- Paffle
     UPDATE silver.tiktok_livestreaming
     SET Studio = 'Klaten'
-    WHERE CreatorId = (SELECT brand_id FROM silver.brand_info WHERE brand_name='Paffle' AND platform='Tiktok')
+    WHERE CreatorId = (SELECT brand_id FROM silver.brand_info WHERE brand_name='Pafle' AND platform='Tiktok')
     AND live_start_date BETWEEN @start_date AND @end_date
-    PRINT 'PAFFLE FILTERED'
+    PRINT 'PAFLE FILTERED'
 
      -- Skinflair
     UPDATE silver.tiktok_livestreaming
