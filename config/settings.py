6b62e7349b27521097bb1f46d6b13891da1b9a21
Yayang NAAW <yayang1123@gmail.com>
2026-07-25 11:13:56 +0700

    NEW CHANGES

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+from dotenv import load_env
+import os
+
+load_env()
+
+DB_SERVER = os.getenv('DB_SERVER')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_DRIVER = os.getenv('DB_DRIVER')
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
