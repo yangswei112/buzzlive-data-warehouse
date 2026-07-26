776eb718640033275c236d21069e8f6568933725
Yayang NAAW <yayang1123@gmail.com>
2026-07-26 15:30:31 +0700

    edit

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,13 @@
-from dotenv import load_dotenv
+# config/settings.py
 import os
+from dotenv import load_dotenv
 
+# Automatically locate and load .env file from the project root
 load_dotenv()
 
-DB_SERVER = os.getenv('DB_SERVER')
-DB_NAME = os.getenv('BuzzliveWarehouse')
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_DRIVER = os.getenv('DB_DRIVER')
+DB_SERVER = os.getenv("DB_SERVER", r"LAPTOP-Q4096V85\SQLEXPRESS")
+DB_NAME = os.getenv("DB_NAME", "BuzzliveWarehouse")
+DB_USERNAME = os.getenv("DB_USERNAME", "de_project")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
