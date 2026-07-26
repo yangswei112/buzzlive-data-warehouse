9ff5212ae45471f4305f4993b3a929e66c2f4c0f
Yayang NAAW <yayang1123@gmail.com>
2026-07-26 15:24:02 +0700

    edit

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 DB_SERVER = os.getenv('DB_SERVER')
+DB_NAME = os.getenv('BuzzliveWarehouse')
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_DRIVER = os.getenv('DB_DRIVER')
