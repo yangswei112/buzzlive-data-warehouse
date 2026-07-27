f250334cbf832f0a14d213c924d555f3a0d112b0
Yayang NAAW <yayang1123@gmail.com>
2026-07-27 09:43:07 +0700

    UPDATE

--- scripts/weekly_pipeline.py
+++ scripts/weekly_pipeline.py
@@ -10,8 +10,8 @@ def extract():
     to extract data from source (shopee & tiktok seller center)
     """
     # Extract livestreaming data from shopee and tiktok seller center
-    shopee_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/buzzlive-data-warehouse/datasets/shopee seller center/to-trf/'
-    tiktok_report_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/buzzlive-data-warehouse/datasets/tiktok seller center/to-trf/'
+    shopee_report_path = 'C:/datasets/shopee seller center/to-trf/'
+    tiktok_report_path = 'C:/datasets/tiktok seller center/to-trf/'
 
     return shopee_report_path, tiktok_report_path
 
@@ -20,7 +20,7 @@ def transform():
     """
     to tansform extracted data to match the database schema
     """
-    result_path = 'C:/Users/ASUS/Documents/Data Engineering/BuzzliveWarehouse/buzzlive-data-warehouse/datasets/result/'
+    result_path = 'C:/datasets/result/'
     shopee_transform_action(extract()[0], result_path)
     tiktok_transform_action(extract()[1], result_path)
 
