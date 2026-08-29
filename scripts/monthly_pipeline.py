81b5c01f6295cea4eaf6effef5538e999626195e
Yayang NAAW <yayang1123@gmail.com>
2026-08-29 10:51:59 +0700

    add new function

--- scripts/monthly_pipeline.py
+++ scripts/monthly_pipeline.py
@@ -16,3 +16,53 @@ def load_shopee_seller_to_sheet(start_date_monthly, end_date_monthly):
 
 def backup_db(month: str):
     backup_database(monthyear=month)
+
+def load_raw_komisi_shopee_all(start_date, end_date, monthyear):
+    """
+    monthyear = 'aug-2026'
+    """
+    gs = GoogleSheetsConn()
+    brands = ['Ortuseight','Beeme','Deltomed','Heavenly Yogurt',
+              'Samyang','Herbana','Herbamojo']
+    for brand in brands:
+        FOLDER_ID_shopee = "1j0Zrd0PzQWhm6tkG-JvJylTFKH5z3KLA"
+        sheet_name_shopee = f"{brand}-gmv-shopee-{monthyear}"
+        df_shopee = get_silver_data_shopee(start_date=start_date, end_date=end_date, brand_name=brand)
+        gs.create_sheet_in_folder(title=sheet_name_shopee, folder_id=FOLDER_ID_shopee)
+        gs.load_dataframe_to_sheet(df = df_shopee, spreadsheet_name=sheet_name_shopee)
+
+def load_raw_komisi_tiktok_all(start_date, end_date, monthyear):
+    """
+    monthyear = 'aug-2026'
+    """
+    gs = GoogleSheetsConn()
+    brands = ['Ortuseight','Beeme','Deltomed',
+              'Medikon','Samyang','Herbana','Herbamojo']
+    for brand in brands:
+        FOLDER_ID_tiktok = "1Gbnl0Nfvq89geo80fjT-1xYhA9kYK1xZ"
+        sheet_name_tiktok = f"{brand}-gmv-tiktok-{monthyear}"
+        df_tiktok = get_silver_data_shopee(start_date=start_date, end_date=end_date, brand_name=brand)
+        gs.create_sheet_in_folder(title=sheet_name_tiktok, folder_id=FOLDER_ID_tiktok)
+        gs.load_dataframe_to_sheet(df = df_tiktok, spreadsheet_name=sheet_name_tiktok)
+
+def load_raw_komisi_tiktok(start_date, end_date, monthyear, brand):
+    """
+    monthyear = 'aug-2026'
+    """
+    gs = GoogleSheetsConn()
+    FOLDER_ID_tiktok = "1Gbnl0Nfvq89geo80fjT-1xYhA9kYK1xZ"
+    sheet_name_tiktok = f"{brand}-gmv-tiktok-{monthyear}"
+    df_tiktok = get_silver_data_shopee(start_date=start_date, end_date=end_date, brand_name=brand)
+    gs.create_sheet_in_folder(title=sheet_name_tiktok, folder_id=FOLDER_ID_tiktok)
+    gs.load_dataframe_to_sheet(df = df_tiktok, spreadsheet_name=sheet_name_tiktok)
+
+def load_raw_komisi_shopee(start_date, end_date, monthyear, brand):
+    """
+    monthyear = 'aug-2026'
+    """
+    gs = GoogleSheetsConn()
+    FOLDER_ID_shopee = "1j0Zrd0PzQWhm6tkG-JvJylTFKH5z3KLA"
+    sheet_name_shopee = f"{brand}-gmv-shopee-{monthyear}"
+    df_shopee = get_silver_data_shopee(start_date=start_date, end_date=end_date, brand_name=brand)
+    gs.create_sheet_in_folder(title=sheet_name_shopee, folder_id=FOLDER_ID_shopee)
+    gs.load_dataframe_to_sheet(df = df_shopee, spreadsheet_name=sheet_name_shopee)
